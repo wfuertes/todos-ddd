65e55f5295f2eaf0b30bccbfbc3227abf32d65b7
Willian Batista <willian.batista@justeattakeaway.com>
2025-01-17 11:05:06 -0700

    Move users db to resources

--- src/main/java/com/wfuertes/infrastructure/ConnectionFactory.java
+++ src/main/java/com/wfuertes/infrastructure/ConnectionFactory.java
@@ -1,20 +1,23 @@
 package com.wfuertes.infrastructure;
 
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Objects;
 
 public class ConnectionFactory {
-    private static final String JDBC_URL = "jdbc:sqlite:/home/willian.batista/IdeaProjects/todos/users-tb.sqlite";
-
     public Connection create() {
         try {
-            var conn = DriverManager.getConnection(JDBC_URL);
+            final URL usersDbUrl = Objects.requireNonNull(ConnectionFactory.class.getResource("/users-tb.sqlite"),
+                                                          "Database file not found");
+            final var jdbcUrl = "jdbc:sqlite:" + usersDbUrl.getPath();
+            var conn = DriverManager.getConnection(jdbcUrl);
             if (conn == null) {
                 throw new SQLException("Connection to the database failed");
             }
             return conn;
-        } catch (SQLException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
